3842da90f6b1e523a9025ebc0b38afa2bc8b905e
NanzeRT <nanotest.vlad@gmail.com>
2022-03-07 22:27:47 +0300

    added logger

--- Controller.py
+++ Controller.py
@@ -1,5 +1,8 @@
+import sys
 import ips
-
+from shutil import rmtree as rmdir
+import os
+import traceback
 
 class Controller:
     def __init__(self) -> None:
@@ -9,8 +12,55 @@ class Controller:
         pass
 
 
+def reset():
+    logger.log('reset')
+    rmdir('SB', ignore_errors=True) # sandbox
+    os.mkdir('SB')
+    if not os.path.isdir('Logs'):
+        os.mkdir('Logs')
+    logger.next_log()
+
+
+class Logger:
+    def __init__(self, path) -> None:
+        self.buffer = []
+        self.path = path
+
+    def log(self, obj):
+        self.buffer += [str(obj)]
+
+    def flush(self):
+        if not self.buffer:
+            return
+        with open(self.path + f"log-{self.get_num()}.txt", 'a') as log_out:
+            log_out.write('log: ' + '\nlog: '.join(self.buffer) + '\n')
+        self.buffer = []
+    
+    def get_num(self) -> int:
+        try:
+            return int(open(self.path + 'log_num.txt').read())
+        except (FileNotFoundError, ValueError):
+            return 0
+    
+    def next_log(self):
+        next = self.get_num() + 1
+        open(self.path + 'log_num.txt', 'w').write(str(next))
+
+
+logger = Logger('Logs/')
+
 if __name__ == '__main__':
-    psm = ips.init()
+    psm = ips.init_test()
+    if psm.tick == 0:
+        reset()
+    logger.log(f'--- tick {psm.tick} ---')
     cont = Controller()
-    cont.run(psm)
-    psm.save_and_exit()
+    try:
+        cont.run(psm)
+    except Exception as e:
+        logger.log(traceback.format_exc())
+        logger.flush()
+        reset()
+        raise e
+    logger.flush()
+    # psm.save_and_exit()
